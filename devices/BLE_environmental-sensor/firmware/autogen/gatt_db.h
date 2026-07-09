d158191ee2d7d9033ea3ea581a61982cbbcfd312
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2026-07-09 17:37:10 +0200

    Updates the device advertised values.

--- devices/BLE_environmental-sensor/firmware/autogen/gatt_db.h
+++ devices/BLE_environmental-sensor/firmware/autogen/gatt_db.h
@@ -25,12 +25,12 @@ extern const sli_bt_gattdb_t gattdb;
 #define gattdb_service_changed_char_len       4
 #define gattdb_database_hash_len              16
 #define gattdb_client_support_features_len    1
-#define gattdb_device_name_len                13
+#define gattdb_device_name_len                10
 #define gattdb_device_information_len         2
-#define gattdb_manufacturer_name_string_len   12
+#define gattdb_manufacturer_name_string_len   11
 #define gattdb_model_number_string_len        10
 #define gattdb_hardware_revision_string_len   5
-#define gattdb_firmware_revision_string_len   8
+#define gattdb_firmware_revision_string_len   10
 #define gattdb_system_id_len                  8
 
 
