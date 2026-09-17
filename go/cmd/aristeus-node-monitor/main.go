1dac73880c25f56c19db577609546f79da4af758
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2026-09-17 16:20:59 +0200

    WIP

--- go/cmd/aristeus-node-monitor/main.go
+++ go/cmd/aristeus-node-monitor/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
+	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/jessevdk/go-flags"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type Options struct {
+	I2CBus            string `long:"i2c-bus" description:"i2c-bus to use" default:"/dev/i2c-0"`
+	PrometheusAddress string `long:"prometheus-address" description:"prometheus address to serve" default:":2112"`
 }
 
 var opts = Options{}
@@ -14,10 +23,65 @@ var opts = Options{}
 var parser = flags.NewParser(&opts, flags.Default)
 
 func main() {
-	if _, err := parser.Parse(); err != nil {
-		if flags.WroteHelp(err) == true {
-			os.Exit(0)
-		}
+
+	if err := execute(); err != nil {
+		slog.Error("unhandled error", slog.String("error", err.Error()))
 		os.Exit(1)
 	}
+}
+
+func emc2101Loop(ctx context.Context, i2cbus string) {
+
+}
+
+func serverPrometheus(ctx context.Context, reg *prometheus.Registry, address string) error {
+	mux := http.NewServeMux()
+
+	mux.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+
+	server := http.Server{
+		Addr:    address,
+		Handler: mux,
+	}
+
+	logger := slog.With(slog.String("module", "prometheus"), slog.String("address", address))
+
+	go func() {
+		<-ctx.Done()
+
+		timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		err := server.Shutdown(timeoutCtx)
+		if err != nil {
+			logger.Error("could not shutdown listener",
+				slog.String("error", err.Error()))
+			return
+		}
+		logger.Info("graceful shutdown")
+
+	}()
+
+	logger.Info("listening")
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Error("server failure", slog.String("error", err.Error()))
+		return err
+	}
+	return nil
+}
+
+func execute() error {
+
+	if _, err := parser.Parse(); err != nil {
+		if flags.WroteHelp(err) == true {
+			return nil
+		}
+		return err
+	}
+
+	reg := prometheus.NewRegistry()
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	return serverPrometheus(ctx, reg, opts.PrometheusAddress)
 }
--- go/go.mod
+++ go/go.mod
@@ -12,6 +12,8 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -20,13 +22,19 @@ require (
 	github.com/mattn/go-isatty v0.0.24 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/mgutz/logxi v0.0.0-20161027140823-aebf8a7d67ab // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/ncruces/go-strftime v1.0.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.24.1 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.70.1 // indirect
+	github.com/prometheus/procfs v0.21.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/sys v0.48.0 // indirect
 	golang.org/x/term v0.46.0 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	modernc.org/libc v1.74.4 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
